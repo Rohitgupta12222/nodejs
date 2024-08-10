c949ce3ce4ffde0cc0c96475a82421987a6f1005
Rohitgupta1222 <rohitg@techsimians.com>
2024-08-10 20:06:44 +0530

    changes in db file

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 // const mongoDbURL = process.env.DB_URL_LOCAL;
 const mongoDbURL = process.env.DB_URL;
 mongoose.connect(`${mongoDbURL}`,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-const db=mongoose.connection;
+const db = mongoose.connection;
 db.on('connected',()=> console.log('mongo database connected'));
 db.on('error',(err)=> console.log('mongo database error ',err));
 db.on('disconnected',()=> console.log('mongo database disconnected'))
